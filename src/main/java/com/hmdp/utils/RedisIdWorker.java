92ec494eecf8d786f6973c5ab7c176c9710d8e77
Zhao Yisen <zhaoyisen119@163.com>
2026-01-20 23:14:45 +0800

    全局唯一id

--- src/main/java/com/hmdp/utils/RedisIdWorker.java
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -12,7 +12,7 @@ public class RedisIdWorker {
     /**
      * 开始时间戳
      */
-    private static final long BEGIN_TIMESTAMP = 1640995200L;
+    private static final long BEGIN_TIMESTAMP = 1640995200L; // 2022-01-01 00:00:00
     /**
      * 序列号的位数
      */
@@ -27,16 +27,16 @@ public class RedisIdWorker {
     public long nextId(String keyPrefix) {
         // 1.生成时间戳
         LocalDateTime now = LocalDateTime.now();
-        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
-        long timestamp = nowSecond - BEGIN_TIMESTAMP;
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC); // 转换为秒级时间戳
+        long timestamp = nowSecond - BEGIN_TIMESTAMP; // 相对时间戳
 
         // 2.生成序列号
-        // 2.1.获取当前日期，精确到天
+        // 2.1.获取当前日期，精确到天，这样每天的极限序列号就不会超过2的32次方，保证了安全性
         String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
         // 2.2.自增长
         long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
 
         // 3.拼接并返回
-        return timestamp << COUNT_BITS | count;
+        return timestamp << COUNT_BITS | count; // 左移32位并与序列号进行或运算
     }
 }
